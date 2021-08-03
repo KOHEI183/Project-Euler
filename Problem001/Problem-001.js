dc6c970f7ef9dfaa93824b20b374a538fb43d229
KOHEI183 <zbtbfuufp@gmail.com>
2021-08-03 09:56:46 +0900

    [fix]001

--- Problem001/Problem-001.js
+++ Problem001/Problem-001.js
@@ -1,13 +1,12 @@
 // 10未満の自然数のうち, 3 もしくは 5 の倍数になっているものは 3, 5, 6, 9 の4つがあり, これらの合計は 23 になる.
 // 同じようにして, 1000 未満の 3 か 5 の倍数になっている数字の合計を求めよ.
 
-function Euler(a,b) {
-  let macCount = a;
-  let anser = b
-  for (let index = 1; index < macCount; index++) {
+function Euler(maxCount, a) {
+  let anser = a
+  for (let index = 1; index < maxCount; index++) {
     if (index % 3 === 0 || index % 5 === 0 ) anser += index;
   }
   return anser;
 }
 
-console.log(Euler(1000,0));
+console.log(Euler(1000, 0));
