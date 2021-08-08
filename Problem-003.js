51c8ffd5abcc5880429898ddb3d5d37431b94568
KOHEI183 <zbtbfuufp@gmail.com>
2021-08-08 12:59:54 +0900

    update

--- Problem-003.js
+++ Problem-003.js
@@ -1,17 +1,31 @@
 // 13195 の素因数は 5, 7, 13, 29 である.
 // 600851475143 の素因数のうち最大のものを求めよ.
 
-function Problem3(num1) {
-  let num = num1;
-  let anser = [];
-  for (let i = 2; num >= 0; i++) {
-    if (Number.isInteger(num/i)) {
-      anser.push(i);
-      num = num / i
-    }
-    console.log(num); //出口を考える
-    if(num === 1 || num === 0 ) return　anser [anser.length -1]
-  }
+// function Problem3(num1) {
+//   let num = num1;
+//   let anser = [];
+//   for (let i = 2; num >= 0; i++) {
+//     if (Number.isInteger(num/i)) {
+//       anser.push(i);
+//       num = num / i
+//     }
+//     console.log(num); //出口を考える
+//     if(num === 1 || num === 0 ) return　anser [anser.length -1]
+//   }
+// }
+
+// console.log(Problem3(600851475143));
+
+let num = 600851475143;
+let max = num
+var i = 2;
+while (i < max) { // 値がiと一緒になるまでまわす。
+  while(max % i === 0) { // 最初の素数が出るまで回す
+		(function(newnum) {
+			max = newnum;
+		})(max / i)
+	}
+	i++;
 }
 
-console.log(Problem3(600851475143));
+console.log(max);
